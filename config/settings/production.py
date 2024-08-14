ce7af12230f8bb939b7bb886a78a266a39a366bb
emmanuelokoye <tricelex@gmail.com>
2024-08-14 20:15:33 +0100

    more env changes

--- config/settings/production.py
+++ config/settings/production.py
@@ -17,7 +17,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["chuckzokoye.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["tests.chuckzokoye.com", "*"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
@@ -208,7 +208,7 @@ sentry_sdk.init(
 # -------------------------------------------------------------------------------
 # Tools that generate code samples can use SERVERS to point to the correct domain
 SPECTACULAR_SETTINGS["SERVERS"] = [
-    {"url": "https://chuckzokoye.com", "description": "Production server"},
+    {"url": "https://tests.chuckzokoye.com", "description": "Production server"},
 ]
 # Your stuff...
 # ------------------------------------------------------------------------------
